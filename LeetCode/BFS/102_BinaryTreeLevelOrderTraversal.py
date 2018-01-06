9b2faff29678bfa0b24d65b5433e626b0bf7aae1
HzCee <huangzixihzx@gmail.com>
2018-01-06 12:23:38 +0800

    LC102

---
+++ LeetCode/BFS/102_BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        depth = 0
+        levelOrderTraversal = []
+        
+        nodeQueue = [[root, depth]]
+        
+        
+        while nodeQueue:
+            node, depth = nodeQueue.pop()
+            if node:
+                if depth + 1 > len(levelOrderTraversal):
+                    levelOrderTraversal.append([])
+                levelOrderTraversal[depth].append(node.val)
+                
+                nodeQueue.append([node.right, depth+1])
+                nodeQueue.append([node.left, depth+1])
+        
+        return levelOrderTraversal
