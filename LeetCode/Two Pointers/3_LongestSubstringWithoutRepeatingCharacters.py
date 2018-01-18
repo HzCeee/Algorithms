f479d9903eabc3d2f895c2636126fdabd3729b34
HzCee <huangzixihzx@gmail.com>
2018-01-17 22:57:47 -0500

    LC3

---
+++ LeetCode/Two Pointers/3_LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        left = right = 0
+        maxLength = 0
+        while right < len(s):
+            if s[right] not in s[left: right]:
+                right += 1
+            else:
+                while s[left] != s[right]:
+                    left += 1
+                left += 1
+            maxLength = max(maxLength, right - left)
+        return maxLength
