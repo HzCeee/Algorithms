59c1ccc56ff97072a43b6eb9b63b31e5576f188b
HzCee <huangzixihzx@gmail.com>
2018-01-17 23:37:40 -0500

    LC11

---
+++ LeetCode/Two Pointers/11_ContainerWithMostWater.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(height) - 1
+        maxVolume = 0
+        
+        while left < right:
+            minHeight = min(height[left], height[right])
+            maxVolume = max(maxVolume, (right - left) * minHeight)
+            if height[left] < height[right]:
+                while left <= len(height) - 1 and height[left] <= minHeight:
+                    left += 1
+            else:
+                while right >= 0 and height[right] <= minHeight:
+                    right -= 1
+                    
+        return maxVolume
