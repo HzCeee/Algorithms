c602321fcb43be71e0813550c5e0963faf9ca15b
HzCee <huangzixihzx@gmail.com>
2018-01-21 10:04:06 -0500

    LC524

--- LeetCode/Two Pointers/524_LongestWordInDictionaryThroughDeleting.py
+++ LeetCode/Two Pointers/524_LongestWordInDictionaryThroughDeleting.py
@@ -5,8 +5,26 @@ class Solution(object):
         :type d: List[str]
         :rtype: str
         """
-        for word in sorted(d, key = lambda w: (-len(w), w)):
-            iterS = iter(s)
-            if all(char in iterS for char in word):
-                return word
+        # for word in sorted(d, key = lambda w: (-len(w), w)):
+        #     iterS = iter(s)
+        #     if all(char in iterS for char in word):
+        #         return word
+        # return ''
+    
+        # index denotes the index of the start element in search s
+        
+        sortedD = sorted(d, key = lambda word: (-len(word), word))
+        for word in sortedD:
+            index = 0
+            isMatch = True
+            for char in word:
+                while index < len(s) and s[index] != char:
+                    index += 1
+                if index < len(s):
+                    index += 1
+                else:
+                    isMatch = False
+                    break
+            if isMatch: return word
+        
         return ''
