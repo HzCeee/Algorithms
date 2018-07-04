32467e86f2139de0d08a9d938488f1f4971b0dfe
HzCee <huangzixihzx@gmail.com>
2018-07-04 22:42:11 +0800

    LC81

---
+++ LeetCode/Binary Search/81_SearchInRotatedSortedArray2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        for num in nums:
+            if num == target:
+                return True
+        
+        return False
