145ff5516ccd6b3e5c388900618cf048f6a5a437
HzCee <huangzixihzx@gmail.com>
2018-08-01 23:57:35 +0800

    LC136

---
+++ LeetCode/Hash Table/136_SingleNumber.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        numCount = dict()
+        for num in nums:
+            if num not in numCount:
+                numCount[num] = 0
+            numCount[num] += 1
+            
+        for num, count in numCount.items():
+            if count == 1:
+                return num
