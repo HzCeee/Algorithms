ee7a19bd0ed5c7a2fa9223c648be5b89c03d3184
HzCee <huangzixihzx@gmail.com>
2018-09-29 23:28:33 -0400

    LC260

---
+++ LeetCode/Bit Manipulation/260_SingleNumber3.py
@@ -0,0 +1,14 @@
+from functools import reduce
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        diff = reduce(lambda x, y: x ^ y, nums, 0) # Get the XOR of the two unique elements.
+        diff &= -diff # Find the last set bit.
+        res = [0, 0]
+        for num in nums:
+            res[num & diff == 0] ^= num
+        return res
+        
